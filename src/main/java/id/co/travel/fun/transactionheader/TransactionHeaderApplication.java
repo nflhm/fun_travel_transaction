0b6ee918151f7d7fa109671574adbe5af501af82
Kevin <gunawankevin54@gmail.com>
2023-04-12 15:59:50 +0700

    dev init

---
+++ src/main/java/id/co/travel/fun/transactionheader/TransactionHeaderApplication.java
@@ -0,0 +1,13 @@
+package id.co.travel.fun.transactionheader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionHeaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionHeaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/travel/fun/transactionheader/TransactionHeaderApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.travel.fun.transactionheader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionHeaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
