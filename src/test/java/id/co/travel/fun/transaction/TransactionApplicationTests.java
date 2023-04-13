029524fb1dd14fe4f485b3b0306dbaf21722b4f8
Kevin <gunawankevin54@gmail.com>
2023-04-13 07:50:17 +0700

    Dev - Change project name

---
+++ src/test/java/id/co/travel/fun/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.travel.fun.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
